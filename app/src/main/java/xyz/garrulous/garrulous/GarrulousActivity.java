4b27961b8856f0fbbb4c3de9f870d021b2298518
mikepierre <mikeypierre84@yahoo.com>
2015-10-23 23:31:20 -0400

    Register Activity Layout
    Create core Register Activity Layout, and created strings via XML for
    layouts. Created GarrulousActivity in which will be the main screen
    when user logins with list view for there current connections.

---
+++ app/src/main/java/xyz/garrulous/garrulous/GarrulousActivity.java
@@ -0,0 +1,21 @@
+package xyz.garrulous.garrulous;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GarrulousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garrulous);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
