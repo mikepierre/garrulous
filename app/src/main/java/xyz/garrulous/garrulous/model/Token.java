bec0789c04620aac00a86efe14f11462cb2d9a44
Richard Meyers <remsolutions2010@gmail.com>
2015-12-05 13:01:52 -0500

    updated token class to handle parser data

--- app/src/main/java/xyz/garrulous/garrulous/model/Token.java
+++ app/src/main/java/xyz/garrulous/garrulous/model/Token.java
@@ -9,17 +9,42 @@ import android.content.SharedPreferences;
 public class Token {
 
     SharedPreferences sharedpreferences;
+    private String message = "";
+    private Boolean error  = false;
 
-    public String getAuthToken() {
+    public String getSharedToken() {
         String token = sharedpreferences.getString("auth_token", "");
         return token;
     }
 
-    public void setAuthToken(String auth_token) {
+    public void setSharedToken(String auth_token) {
         SharedPreferences.Editor editor = sharedpreferences.edit();
         editor.putString("auth_token", auth_token);
         editor.commit();
     }
 
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    public String getToken() {
+        return this.getSharedToken();
+    }
+
+    public void setToken(String token) {
+        this.setSharedToken(token);
+    }
 }
