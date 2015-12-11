d9ebc7d63e526e46c85448a610664c8113099bf4
Richard Meyers <remsolutions2010@gmail.com>
2015-12-10 21:22:16 -0500

    Added back button to user searching

--- app/src/main/java/xyz/garrulous/garrulous/Activities/FindUserActivity.java
+++ app/src/main/java/xyz/garrulous/garrulous/Activities/FindUserActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import xyz.garrulous.garrulous.R;
@@ -16,8 +17,21 @@ public class FindUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Find Someone");
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_backspace_white_24dp);
         setSupportActionBar(toolbar);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
