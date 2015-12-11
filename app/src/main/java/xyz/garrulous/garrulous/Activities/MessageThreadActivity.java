fd436997b06026d79b221bf31ac4afa47e333236
Richard Meyers <remsolutions2010@gmail.com>
2015-12-11 02:17:07 -0500

    back button in thread view

--- app/src/main/java/xyz/garrulous/garrulous/Activities/MessageThreadActivity.java
+++ app/src/main/java/xyz/garrulous/garrulous/Activities/MessageThreadActivity.java
@@ -2,6 +2,8 @@ package xyz.garrulous.garrulous.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import xyz.garrulous.garrulous.R;
 
@@ -11,5 +13,21 @@ public class MessageThreadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_thread);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Thread");
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_backspace_white_24dp);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
